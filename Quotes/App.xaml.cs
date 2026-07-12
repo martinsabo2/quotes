f2b915d48b9085efe66c7263904e9a9fe0d96cde
“Martin <martinsabo2@gmail.com>
2026-07-12 15:28:00 +0200

    added a tray icon

--- Quotes/App.xaml.cs
+++ Quotes/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Forms;
 using PowerModeChangedEventArgs = Microsoft.Win32.PowerModeChangedEventArgs;
@@ -9,7 +13,12 @@ namespace Quotes;
 public partial class App : System.Windows.Application
 {
     private NotifyIcon? _notifyIcon;
+    private Icon? _trayAppIcon;
     private AppConfig _config = AppConfig.Load();
+
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool DestroyIcon(nint hIcon);
 
     protected override void OnStartup(StartupEventArgs e)
     {
@@ -28,7 +37,7 @@ public partial class App : System.Windows.Application
     private void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
     {
         // if (e.Mode == PowerModes.Resume)
-            Dispatcher.Invoke(TryShowDailyQuote);
+        Dispatcher.Invoke(TryShowDailyQuote);
     }
 
     private void TryShowDailyQuote()
@@ -74,15 +83,70 @@ public partial class App : System.Windows.Application
         contextMenu.Items.Add(new ToolStripSeparator());
         contextMenu.Items.Add("Exit", null, (_, _) => ExitApp());
 
+        _trayAppIcon = CreateModernQTrayIcon();
+
         _notifyIcon = new NotifyIcon
         {
-            Icon = System.Drawing.SystemIcons.Information,
-            Text = "Quotes – Quote of the Day",
+            Icon = _trayAppIcon,
+            Text = "Quote of the Day",
             ContextMenuStrip = contextMenu,
             Visible = true
         };
 
         _notifyIcon.DoubleClick += (_, _) => ShowQuoteWindow();
+    }
+
+    private static Icon CreateModernQTrayIcon()
+    {
+        const int iconSize = 64;
+        var beige = Color.FromArgb(245, 231, 214);
+        var textColor = Color.FromArgb(46, 37, 30);
+
+        using var bitmap = new Bitmap(iconSize, iconSize, PixelFormat.Format32bppArgb);
+        using var graphics = Graphics.FromImage(bitmap);
+        graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+        graphics.Clear(Color.Transparent);
+
+        var tileSize = iconSize - 6;
+        var tileRect = new Rectangle(3, 3, tileSize, tileSize);
+
+        using (var shadowBrush = new SolidBrush(Color.FromArgb(38, 0, 0, 0)))
+        {
+            graphics.FillEllipse(shadowBrush, tileRect.X + 1, tileRect.Y + 2, tileRect.Width - 2, tileRect.Height - 2);
+        }
+
+        using (var backBrush = new SolidBrush(beige))
+        {
+            graphics.FillEllipse(backBrush, tileRect);
+        }
+
+        using (var borderPen = new Pen(Color.FromArgb(210, 195, 176), 2f))
+        {
+            graphics.DrawEllipse(borderPen, tileRect);
+        }
+
+        using var font = new System.Drawing.Font("Aharoni", 50, System.Drawing.FontStyle.Bold, GraphicsUnit.Pixel);
+        var textRect = new RectangleF(0, 5f, iconSize, iconSize);
+        using var textBrush = new SolidBrush(textColor);
+        using var textFormat = new StringFormat
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center
+        };
+
+        graphics.DrawString("Q", font, textBrush, textRect, textFormat);
+
+        var hIcon = bitmap.GetHicon();
+        try
+        {
+            using var generatedIcon = Icon.FromHandle(hIcon);
+            return (Icon)generatedIcon.Clone();
+        }
+        finally
+        {
+            DestroyIcon(hIcon);
+        }
     }
 
     private void EnsureQuotesFolderHasSamples()
@@ -109,6 +173,7 @@ public partial class App : System.Windows.Application
     {
         SystemEvents.PowerModeChanged -= OnPowerModeChanged;
         _notifyIcon?.Dispose();
+        _trayAppIcon?.Dispose();
         Shutdown();
     }
 
@@ -116,6 +181,7 @@ public partial class App : System.Windows.Application
     {
         SystemEvents.PowerModeChanged -= OnPowerModeChanged;
         _notifyIcon?.Dispose();
+        _trayAppIcon?.Dispose();
         base.OnExit(e);
     }
 }
--- README.md
+++ README.md
@@ -12,3 +12,6 @@ Application displays a quote of the day once a day, the first time I start or aw
 The quotes are stored as text files in a folder that can be configured.
 The application will randomly select a quote from the folder and display it in a window.
 The quote text is selectable and can be copied to the clipboard.
+The application runs in the system tray.
+
+The application shows a tray icon in the system tray, and when clicked, it will show the quote of the day in a window.
